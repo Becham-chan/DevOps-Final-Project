8ccd36e6a922f7366971962cb6b3d3037abae73d
Becham-chan <111549621+Becham-chan@users.noreply.github.com>
2026-05-20 14:36:25 +0700

    shell with data mocking

--- backend/start.sh
+++ backend/start.sh
@@ -2,4 +2,5 @@
 python manage.py collectstatic --noinput
 python manage.py migrate
 python create_superuser.py
+python manage.py populate_mock_data
 exec gunicorn config.wsgi:application --bind 0.0.0.0:${PORT:-10000} --workers 2
